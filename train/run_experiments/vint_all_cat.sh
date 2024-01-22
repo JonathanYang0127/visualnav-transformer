300eda2b5dff47c03cf769c4b68729ed2c66e99d
Catherine Glossop <catherine_glossop@berkeley.edu>
2024-01-22 12:15:46 -0800

    restart 160 run

--- train/run_experiments/vint_all_cat.sh
+++ train/run_experiments/vint_all_cat.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=savio4_gpu
 #SBATCH --account=co_rail
-#SBATCH --time=1-12:00:00
+#SBATCH --time=1-20:00:00
 #SBATCH --job-name=catjob
 #SBATCH --qos=rail_gpu4_normal
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=150G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=175G
 #SBATCH --gres=gpu:A5000:1
 #SBATCH --job-name="cglossop_omni"
 #SBATCH --output=/global/scratch/users/catherineglossop/omnimimic/outputs/exp-%j.out
